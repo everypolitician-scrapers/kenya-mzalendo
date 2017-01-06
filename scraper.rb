5d52da807388f6aa07c2aac2f81fc004d036a136
Tony Bowden <tony@tmtm.com>
2017-01-06 16:53:19 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -15,7 +16,7 @@ OpenURI::Cache.cache_path = '.cache'
 require 'scraped_page_archive/open-uri'
 
 def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
+  Nokogiri::HTML(open(url).read)
 end
 
 def date_from(str)
@@ -28,10 +29,10 @@ def scrape_list(url)
   puts url
   noko = noko_for(url)
   noko.css('.position-listing a[href*="/person/"]/@href').each do |p|
-    scrape_person(URI.join url, p.text)
+    scrape_person(URI.join(url, p.text))
   end
   next_page = noko.css('.pagination a.next/@href').text
-  scrape_list(URI.join url, next_page) unless next_page.empty?
+  scrape_list(URI.join(url, next_page)) unless next_page.empty?
 end
 
 def scrape_experience(url)
@@ -41,7 +42,7 @@ def scrape_experience(url)
   member_element = experience.css('li.position').xpath('.//h4[contains(.,"Member of the National Assembly")]')
   date_and_area = member_element.xpath('.//following-sibling::p[1]')
   start_text, end_text = date_and_area.text.match(/(.*)\s*→\s*(.*)/).captures rescue binding.pry
-  return date_from(start_text), date_from(end_text), date_and_area.css('a')
+  [date_from(start_text), date_from(end_text), date_and_area.css('a')]
 end
 
 def scrape_person(url)
@@ -50,18 +51,18 @@ def scrape_person(url)
   sidebar = noko.css('div.constituency-party')
   area = sidebar.at_xpath('.//a[contains(@href,"/place/")]')
 
-  party_node = sidebar.xpath('.//a[contains(@href,"/organisation/")]').find { |n|
+  party_node = sidebar.xpath('.//a[contains(@href,"/organisation/")]').find do |n|
     org_url = URI.join(url, n.attr('href')).to_s
     noko_org = noko_for(org_url)
     type = noko_org.css('div.object-titles p').text == 'Political Party'
-  }
+  end
   party_info = party_node ? party_node.text.strip : 'Independent (IND)'
   party_data = party_info.match(/(.*) \((.*)\)/)
   party, party_id = party_data ? party_data.captures : [party_info, nil]
 
   experience = noko.css('div.person-detail-experience')
   member_element = experience.css('li.position').xpath('.//h4[contains(.,"Member of the National Assembly")]')
-  if member_element.size > 0
+  if !member_element.empty?
     start_text = member_element.xpath('.//following-sibling::p[@class="position-date" and contains(.,"Started")][1]').text rescue nil
     start_date = date_from(start_text.gsub('Started ', ''))
     end_date = ''
@@ -74,38 +75,38 @@ def scrape_person(url)
   end
 
   subtitle = member_element.xpath('.//following-sibling::p[@class="position-subtitle"][1]').text rescue nil
-  if subtitle.match('Women')
-    rep_type = "Women's Representative"
-  elsif subtitle.match('Nominated')
-    rep_type = "Nominated Representative"
-  else
-    rep_type = ''
-  end
+  rep_type = if subtitle.match('Women')
+               "Women's Representative"
+             elsif subtitle.match('Nominated')
+               'Nominated Representative'
+             else
+               ''
+             end
   contacts = noko.css('.contact-details')
 
   alt_name = contacts.xpath('.//h3[contains(.,"Full name")]/following-sibling::p[1]').text rescue nil
 
   data = {
-    id: url.to_s[/person\/(.*)\//, 1],
-    name: noko.css('div.object-titles h1').text.gsub(/[[:space:]]+/, ' ').strip,
-    party: party,
-    party_id: party_id,
-    area: area ? area.text.strip : '',
-    email: contacts.css('a[href*="mailto:"]/@href').map(&:text).first.to_s.sub('mailto:',''),
-    birth_date: date_from(contacts.xpath('.//h3[contains(.,"Born")]/following-sibling::p[1]').text),
-    facebook: contacts.css('a[href*="facebook"]/@href').map(&:text).first.to_s,
-    twitter: contacts.css('a[href*="twitter"]/@href').map(&:text).first.to_s,
-    term: '11',
-    image: noko.css('.profile-pic img/@src').text,
-    source: url.to_s,
-    start_date: start_date,
-    end_date: end_date,
-    identifier__mzalendo: noko.at_css('meta[name="pombola-person-id"]/@content').text,
+    id:                          url.to_s[/person\/(.*)\//, 1],
+    name:                        noko.css('div.object-titles h1').text.gsub(/[[:space:]]+/, ' ').strip,
+    party:                       party,
+    party_id:                    party_id,
+    area:                        area ? area.text.strip : '',
+    email:                       contacts.css('a[href*="mailto:"]/@href').map(&:text).first.to_s.sub('mailto:', ''),
+    birth_date:                  date_from(contacts.xpath('.//h3[contains(.,"Born")]/following-sibling::p[1]').text),
+    facebook:                    contacts.css('a[href*="facebook"]/@href').map(&:text).first.to_s,
+    twitter:                     contacts.css('a[href*="twitter"]/@href').map(&:text).first.to_s,
+    term:                        '11',
+    image:                       noko.css('.profile-pic img/@src').text,
+    source:                      url.to_s,
+    start_date:                  start_date,
+    end_date:                    end_date,
+    identifier__mzalendo:        noko.at_css('meta[name="pombola-person-id"]/@content').text,
     legislative_membership_type: rep_type,
   }
   data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
   data[:alternate_names] = alt_name unless alt_name.to_s.empty?
-  ScraperWiki.save_sqlite([:id, :term], data)
+  ScraperWiki.save_sqlite(%i(id term), data)
 end
 
 scrape_list('http://info.mzalendo.com/position/member-national-assembly/governmental/parliament/?session=na2013')
