d0c826dfe098a3d72c44a4046c4dd4e9b23f4797
Tony Bowden <tony@tmtm.com>
2017-04-13 14:42:29 +0100

    Turn off archiving
    This is less important than it would be for an official site, and the
    scraper falls over as often as it completes, so let's see if this helps.

--- scraper.rb
+++ scraper.rb
@@ -6,9 +6,9 @@ require 'date'
 require 'scraped'
 require 'scraperwiki'
 
-# require 'open-uri/cached'
-# OpenURI::Cache.cache_path = '.cache'
-require 'scraped_page_archive/open-uri'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+# require 'scraped_page_archive/open-uri'
 
 def noko_for(url)
   Nokogiri::HTML(open(url).read)
